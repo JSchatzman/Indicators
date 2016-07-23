7b8b142ade4633465a686f8ca0f508f20f656f88
JSchatzman <schatty310@hotmail.com>
2016-07-22 22:26:40 -0700

    Gfdgd

--- static/indicators.js
+++ static/indicators.js
@@ -54,7 +54,7 @@ $(document).ready(function () {
 	$('#main_submit').click(function() {
 		var sy = $('#start_year').val();
 		var ey = $('#end_year').val();
-		var region = $('#region_anchor');
+		var region = $('#region_anchor').val();
 		if(!validYears(sy, ey)) {
 			alert("Please enter valid start and end years");
 			return;
